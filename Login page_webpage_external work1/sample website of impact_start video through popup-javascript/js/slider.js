cb487d1fb7178203c01e602e687badacadf439a5
DhavalKanjariya123 <118043860+DhavalKanjariya123@users.noreply.github.com>
2023-01-09 12:30:22 +0530

    Add files via upload

---
+++ Login page_webpage_external work1/sample website of impact_start video through popup-javascript/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".impact-slider").cycle("fade");
+})
+
+$(document).ready(function () {
+
+    $(".symbol-btn").click(function () {
+        $(".video").slideToggle(500);
+
+    })
+})
