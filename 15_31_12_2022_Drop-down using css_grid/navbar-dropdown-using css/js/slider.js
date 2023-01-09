8050df01294ef8d0c9601dd40498f638916981d6
DhavalKanjariya123 <118043860+DhavalKanjariya123@users.noreply.github.com>
2023-01-09 12:14:35 +0530

    Add files via upload

---
+++ 15_31_12_2022_Drop-down using css_grid/navbar-dropdown-using css/js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".impact-slider").cycle("fade");
+})
+
+$(document).ready(function () {
+
+    $(".symbol-btn").click(function () {
+        $(".video").slideToggle(500);
+
+    })
+})
+$(document).ready(function () {
+
+    $(".impact-team").click(function () {
+        $(".impact-team-all").slideToggle(300);
+    })
+})
