5bb96805ce34bfb9ab7e3f7da687a6030febd364
Dhaval Kanjariya <118043860+DhavalKanjariya123@users.noreply.github.com>
2023-04-21 18:31:36 +0530

    Add files via upload

---
+++ 26_11_2_2023_calculator(addition,substraction,multipication,modulud,clear)/Calculator/js/main.js
@@ -0,0 +1,24 @@
+function clc(val) {
+    document.getElementById("result").value += val;
+}
+function clean() {
+    document.getElementById("result").value = "";
+}
+function finaloperat() {
+    let x = document.getElementById("result").value;
+    let y = document.getElementById("result").value;
+    x = eval(y);
+    document.getElementById("result").value = x;
+}
+
+function square() {
+    var a=document.getElementById("result").value;
+    var b=a*a;
+    document.getElementById("result").value=b;
+
+}
+
+function del() {
+    var a=document.getElementById("result").value;
+    document.getElementById("result").value=a.slice(0,- 1);
+}
